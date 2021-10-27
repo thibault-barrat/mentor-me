981d6d2dfe35ad6024b281de91ee316842b89366
Davnc988 <davdang988@gmail.com>
2021-10-27 16:29:37 +0200

    creation of connect component

--- front/src/pages/Connect/index.js
+++ front/src/pages/Connect/index.js
@@ -1,6 +1,33 @@
+import { Link } from 'react-router-dom';
+import Field from './Field';
+
+import './style.scss';
+
 const Connect = () => (
-  <div>
-    <h1>Connect</h1>
+  <div className="connect">
+    <form >
+      <Field 
+        name="email"
+        placeholder="Email"
+        
+      />
+      <Field 
+        name="password"
+        placeholder="Mot de passe"
+        
+      />
+      <button
+        className="connect-button"
+      >
+      Me connecter
+      </button>
+    </form>
+    <Link 
+      className="connect-link"
+      to="/inscription"
+    >
+    Vous n'avez pas de compte ?
+    </Link>
   </div>
 );
 
