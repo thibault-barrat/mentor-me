01128bbcfb278346fdadaaca784289d43eea46c5
thibault-barrat <contact@thibault-barrat.com>
2021-10-27 16:47:18 +0200

    design of home for not connected users

--- front/src/components/NavBaar/MenuItems.js
+++ front/src/components/NavBaar/MenuItems.js
@@ -1,22 +1,25 @@
 // eslint-disable-next-line import/prefer-default-export
 export const MenuItems = [
   {
+    id: 1,
     title: 'Accueil',
     url: '/',
     cName: 'navLinks',
   },
   {
+    id: 2,
     title: 'A propos',
     url: '/a-propos',
     cName: 'navLinks',
   },
   {
-    // eslint-disable-next-line quotes
-    title: `S'inscrire`,
+    id: 3,
+    title: 'S\'inscrire',
     url: '/inscription',
     cName: 'navLinks',
   },
   {
+    id: 4,
     title: 'Se connecter',
     url: '/connexion',
     cName: 'navLinks',
