57be18614bf90c7d367c86c564d01a3b9844ee76
FloClem <flo799@live.fr>
2021-10-27 13:36:37 +0200

    NavBar done with structure of reactRouter

--- front/src/App.js
+++ front/src/App.js
@@ -1,23 +1,22 @@
-import React from 'react';
-import Navbar from './components/NavBar';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import Home from './pages';
-import About from './pages/about';
-import Register from './pages/register';
-import Connect from './pages/connect';
+import Home from './pages/Home';
+import About from './pages/About';
+import Register from './pages/Register';
+import Connect from './pages/Connect';
+import NavBaar from './components/NavBaar';
 
 function App() {
-return (
-	<Router>
-    <Navbar />
-    <Switch>
-      <Route path='/' exact component={Home} />
-      <Route path='/A-propos' component={About} />
-      <Route path='/inscription' component={Register} />
-      <Route path='/connexion' component={Connect} />
-    </Switch>
-	</Router>
-);
+  return (
+    <Router>
+      <NavBaar />
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/A-propos" component={About} />
+        <Route path="/inscription" component={Register} />
+        <Route path="/connexion" component={Connect} />
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
