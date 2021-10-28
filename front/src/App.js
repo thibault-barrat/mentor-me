d8dbdce02c19d9eec1bec0255f0afe88263f1ab6
thibault-barrat <contact@thibault-barrat.com>
2021-10-28 10:11:11 +0200

    create reducers for services and categories

--- front/src/App.js
+++ front/src/App.js
@@ -1,4 +1,6 @@
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect } from 'react';
 import Home from './pages/Home';
 import About from './pages/About';
 import Register from './pages/Register';
@@ -7,6 +9,11 @@ import NavBaar from './components/NavBaar';
 import Footer from './components/Footer';
 
 function App() {
+  // We need to know if the user is logged and if is admin
+  // to enable or disable specific routes
+  const isLogged = useSelector((state) => state.user.logged);
+  const isAdmin = useSelector((state) => state.user.isAdmin);
+  const dispatch = useDispatch();
   return (
     <Router>
       <NavBaar />
