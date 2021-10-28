ecd4a327dcd20f1f3a8edae795117bfc6fd54eb1
MarionBricout <marion.bricout@protonmail.com>
2021-10-28 20:11:01 +0200

    setting up cors

--- back/index.js
+++ back/index.js
@@ -1,8 +1,17 @@
 require("dotenv").config({ path: "./.env" });
 const express = require("express");
+const cors = require("cors");
 const app = express();
 
+app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+app.use(cors());
+// penser à mettre cors(corsOptions) ligne 10
+// const corsOptions = {
+//   origin: 'http://example.com',
+//   optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+// }
 
 const router = require("./app/router");
 app.use(router);
