0ddf0f7fef17cb8123960f237b3a5b9e5d68776e
thibault-barrat <contact@thibault-barrat.com>
2021-11-05 09:48:55 +0100

    quickfix on first render

--- front/src/App.js
+++ front/src/App.js
@@ -28,14 +28,14 @@ function App() {
   // At the first render of App, we fetch categories and services
   // we checked if there is a token in localStorage
   // if there is, we set the user as logged
-  // useEffect(() => {
-  //   dispatch(fetchCategories());
-  //   dispatch(fetchServices());
-  //   const token = localStorage.getItem('token');
-  //   if (token) {
-  //     dispatch({ type: 'SET_LOGGED', payload: true });
-  //   }
-  // }, []);
+  useEffect(() => {
+    dispatch(fetchCategories());
+    dispatch(fetchServices());
+    // const token = localStorage.getItem('token');
+    // if (token) {
+    //   dispatch({ type: 'SET_LOGGED', payload: true });
+    // }
+  }, []);
 
   return (
     <Router>
